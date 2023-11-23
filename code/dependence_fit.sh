4469fc9831d7e74eb74ed9f89878fa4c2362f678
PangChung <zhongprw@gmail.com>
2023-11-23 20:30:23 +1100

    fit dep

--- code/dependence_fit.sh
+++ code/dependence_fit.sh
@@ -5,25 +5,29 @@ cd ~/Desktop/ExtremePrecip/
 declare -a pids
 
 # Maximum number of parallel jobs
-max_jobs=8
+max_jobs=4
 
 # Function to run a job with nohup
 run_job() {
     local region=$1
     local season=$2
     local norm=$3
-    nohup Rscript code/dependence_fit.R "idx.region=$region;season.ind=$season;norm.ind=$3" &
+    local output_file="output_${region}_${season}_${norm}.txt"
+    #local error_file="error_${region}_${season}_${norm}.txt"
+    nohup Rscript code/dependence_fit.R "idx.region=$region;season.ind=$season;norm.ind=$norm" > "$output_file" 2>&1  &
+    echo $!
     pids+=($!)
 }
 
 # Run multiple jobs in parallel
-for region in {1..8}; do
+for region in {2..8}; do
     for season in {1..4}; do
         for norm in {1..2}; do
             # Check if the maximum number of parallel jobs has been reached
             while [ ${#pids[@]} -ge $max_jobs ]; do
                 sleep 1
             done
+            echo "Running job $region $season $norm"
             run_job $region $season $norm
         done
     done
