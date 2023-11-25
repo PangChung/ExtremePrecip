57223f55228d656fd51fdba42de95ebecd744e9c
PangChung <zhongprw@gmail.com>
2023-11-26 10:39:07 +1100

    bug fix

--- code/collect_results.R
+++ code/collect_results.R
@@ -1,3 +1,4 @@
+load("data/precip.RData")
 marginal.fit.files <- list.files("data/",pattern = "marginal_fit_.*.RData",full.names = TRUE)
 dep.fit.files <- list.files("data/",pattern="fit_pot_ST_*",full.names = TRUE)
 
@@ -6,13 +7,12 @@ for(i in 1:length(marginal.fit.files)){
     print(is.null(e$data.df.gpd))
 }
 
-time.vec <- c()
-time.elapse <- c()
-estimates <- c()
 
+results.list <- list()
 for(i in 1:length(dep.fit.files)){
     load(dep.fit.files[i],e1<-new.env())
-    time.vec <- c(time.vec,e1$result$time[5])
-    time.elapse <- c(time.elapse,e1$result$time[3])
-    estimates <- rbind(estimates,e1$result$par)
+    e1$file <- dep.fit.files[i]
+    results.list[[i]] <- e1
 }
+
+save(results.list,file="data/dep.fit.results.RData")
