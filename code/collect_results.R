abe4ef82a77ef5bf179cba91e0757eacc04cda95
PangChung <zhongprw@gmail.com>
2024-02-06 21:43:04 +1100

    update

--- code/collect_results.R
+++ code/collect_results.R
@@ -16,3 +16,28 @@ for(i in 1:length(dep.fit.files)){
 }
 
 save(results.list,file="data/dep.fit.results.RData")
+
+
+### collect results for bootstrap ### 
+library(stringr)
+boot.files <- list.files(path="data/bootstrap/",pattern="fit_bootstrap_",full.names = TRUE)
+region.idx <- as.numeric(str_extract(str_extract(boot.files,"_[1-8]\\."),"[1-8]"))
+boot.idx <- as.numeric(str_extract(str_extract(boot.files,"_\\d+_"),"\\d+"))
+
+results.boot.list <- list()
+for(i in 1:length(boot.files)){
+    load(boot.files[i],e<-new.env())
+    results.boot.list[[i]] <- e$result.list
+}
+
+save(results.boot.list,region.idx, boot.files, boot.idx,file="data/dep.fit.boot.results.RData")
+
+jackknife <- function(data,idx,norm.idx,season.idx,region.idx,boot.idx){
+    data = results.boot.list;norm.idx=1;season.idx=1;idx = 1
+    estimates.boot = matrix(unlist(lapply(data[region.idx==idx & boot.idx != 301],function(x){x[[norm.idx]][[season.idx]]$par})),ncol=3,byrow=TRUE)
+    est.true = results.boot.list[region.idx == idx  & boot.idx == 301][[1]][[norm.idx]][[season.idx]]$par
+    n = nrow(estimates.boot)
+    est.jackknife = n * est.true  - (n-1) * estimates.boot
+    apply(est.jackknife,2,mean)
+}
+
