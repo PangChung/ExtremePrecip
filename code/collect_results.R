8fd47e6a00e78a250481543322b2dea36fa94bf5
PangChung <zhongprw@gmail.com>
2024-02-14 00:07:23 +1100

    bug fix

--- code/collect_results.R
+++ code/collect_results.R
@@ -27,7 +27,7 @@ jackknife <- function(data,idx,norm.idx,season.idx,region.idx,boot.idx){
     estimates.boot = matrix(unlist(lapply(data[region.idx==idx & boot.idx != 301],function(x){x[[norm.idx]][[season.idx]]$par})),ncol=3,byrow=TRUE)
     est.true = results.boot.list[region.idx == idx  & boot.idx == 301][[1]][[norm.idx]][[season.idx]]$par
     n = nrow(estimates.boot)
-    sd.jackknife = apply(estimates.boot,2,function(x){return(sd(x)*(n-1)/sqrt(n))})
+    sd.jackknife = apply(estimates.boot,2,function(x){return(sd(x))})
     return(list(sd=sd.jackknife,true=est.true,jack=estimates.boot))
 }
 
@@ -41,4 +41,6 @@ for(norm.idx in 1:2){
         jack.result.list[[norm.idx]][[season.idx]] <- result.list
     }
 }
+
+#norm.ind;season.idx;idx.region
 str(jack.result.list)
